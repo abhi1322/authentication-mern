21967327cc336b3e1b6d651afc375334da34dbbf
Abhishek Kumar <abhi1322jii@gmail.com>
2024-08-11 18:45:40 +0530

    final commit

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,10 +1,70 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login",
+        loginData
+      );
+
+      const { success, message } = response.data;
+
+      if (success) {
+        console.log("login successfull");
+      } else {
+        console.log(message);
+      }
+      setLoginData({ username: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={loginData.username}
+          onChange={handleLoginChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={loginData.password}
+          onChange={handleLoginChange}
+        />
 
-export default Login
+        <button type="submit">Login</button>
+
+        <p>
+          Not registered yet? <span> </span>
+          <Link to="/register">Sign Up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/screens/Register.jsx
+++ client/src/screens/Register.jsx
@@ -1,8 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
 
-export default Register
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user);
+
+    try {
+      const response = await axios.post("http://localhost:3000/register", user);
+
+      const { suceess, message } = response.data;
+      if (suceess) {
+        console.log("register successfull");
+      } else {
+        console.log(message);
+      }
+      window.location.href = "http://localhost:5173/login";
+      // redirect("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={user.username}
+          onChange={handleLoginChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={user.password}
+          onChange={handleLoginChange}
+        />
+        <button type="submit">Login</button>
+
+        <p>
+          Not registered yet? <span> </span>
+          <Link to="/register">Sign Up</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
